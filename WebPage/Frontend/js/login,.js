b14e66ddc29f8371c3918d09b1215ad86c9d9ade
Lederback <renatosmachado2@gmail.com>
2022-12-15 11:24:41 -0300

    Backend, Frontend e integração finalizados

--- WebPage/Frontend/js/login,.js
+++ WebPage/Frontend/js/login,.js
@@ -1,13 +1,12 @@
 var url = "http://10.128.64.59:2707";
 
+//Function to check if credentials are in the database
 function validateLogin(){
 
     $.get(url + "/getLogin/" + $(".username").val() + "/" + $(".password").val(), function (resultado) {
         var objeto = JSON.parse(resultado);
 
-        if (Object.keys(objeto).length == 0){
-
-        }
+        if (Object.keys(objeto).length == 0){}
         else{
             window.location = url + "/produtividade.html";
         }
